d380ab7386b2eaff751e0bd157acf8cf1598fb3b
mw491 <musaiw@outlook.com>
2022-12-07 22:05:22 +0300

    Add page to view and delete chirps to resolve #10

--- chirps/views.py
+++ chirps/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import Chirp
 
-# Create your views here.
+
+def view_post(request, username, id):
+    if request.POST:
+        Chirp.objects.filter(id=id).delete()
+        messages.success("Post successfully deleted")
+        return redirect("ui-home")
+    author = User.objects.get(username=username)
+    chirp = Chirp.objects.get(id=id, author=author)
+
+    return render(request, "chirps/view_chirp.html", {"chirp": chirp, "user": request.user})
--- ui/urls.py
+++ ui/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from chirps import views as chirp_views
 
 urlpatterns = [
     path("", views.home, name="ui-home"),
+    path('<str:username>/chirp/<str:id>', chirp_views.view_post, name='view-chirp'),
 ]
